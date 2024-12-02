c10d1030c8a7bb6773fa285a2516986d8ed19245
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-12-02 11:41:41 +0530

    add web manifest and update environment configuration for new API endpoints

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,9 +3,9 @@ export const environment = {
   socketUrl:
     window.location.hostname === 'localhost'
       ? 'http://localhost:3000/socket'
-      : 'https://code-sharing-app-t6fq.vercel.app/socket',
+      : 'https://code-sharing-app-api.vercel.app//socket',
   apiUrl:
     window.location.hostname === 'localhost'
       ? 'http://localhost:3000/api'
-      : 'https://https://code-sharing-app-t6fq.vercel.app/api',
+      : 'https://https://code-sharing-app-api.vercel.app//api',
 };
