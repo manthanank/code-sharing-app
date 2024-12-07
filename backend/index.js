b893d9cee54f6cc56173761598a8c057d1bf3331
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-12-07 21:31:24 +0530

    fix: update CORS configuration to allow requests from any origin

--- backend/index.js
+++ backend/index.js
@@ -9,27 +9,14 @@ dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server, {
+const io = socketIo(server,{
   cors: {
-    origin: [
-      "http://localhost:3000",
-      "https://code-sharing-app-manthanank.vercel.app",
-    ],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
-    credentials: true,
   },
 });
 
-app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "https://code-sharing-app-manthanank.vercel.app",
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type"],
-  credentials: true,
-}));
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 // MongoDB connection
